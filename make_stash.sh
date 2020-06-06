f5222bf69af863f7379e6a80252ef08940f10115
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-06-07 00:56:10 +0900

    Update make_stash.sh

--- make_stash.sh
+++ make_stash.sh
@@ -3,3 +3,4 @@ git stash clear
 curl https://raw.githubusercontent.com/usagrammer/git-exam/curl_raw_files/app/models/message.rb >> app/models/message.rb
 git add .
 git stash save "!!GitHub_Desktop<`git rev-parse --abbrev-ref HEAD`>"
+pkill -f Terminal
