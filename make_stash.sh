8cb46c35e21112945f787eefa45a957d7c40afeb
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-06-07 01:10:11 +0900

    Create make_stash.sh

---
+++ make_stash.sh
@@ -0,0 +1,6 @@
+# touch `date +"%Y-%m-%d_%H:%M:%S"`.txt
+git stash clear
+curl https://raw.githubusercontent.com/usagrammer/git-exam/curl_raw_files/app/models/message.rb >> app/models/message.rb
+git add .
+git stash save "!!GitHub_Desktop<`git rev-parse --abbrev-ref HEAD`>"
+killall Terminal
